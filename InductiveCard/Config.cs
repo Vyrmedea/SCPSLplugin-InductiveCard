a0871244abbd79b17088571206ebb486c17c1c0a
Vyrmedea <noilndygs@gmail.com>
2023-06-20 01:25:34 +0800

    添加项目文件。

---
+++ InductiveCard/Config.cs
@@ -0,0 +1,10 @@
+﻿namespace InductiveCard
+{
+    using Exiled.API.Interfaces;
+
+    public class Config:IConfig
+    {
+        public bool IsEnabled { get; set; }=true;
+        public bool Debug { get; set; }=false;
+    }
+}
---
+++ InductiveCard/Plugin.cs
@@ -0,0 +1,25 @@
+﻿namespace InductiveCard
+{
+    using Exiled.API.Features;
+    using PlayerHandlers = Exiled.Events.Handlers.Player;
+    using ServerHandlers = Exiled.Events.Handlers.Server;
+    public class Plugin:Plugin<Config>
+    {
+        private static readonly Plugin SingletonInstance = new Plugin();
+        private Plugin() { }
+        public static Plugin Instance => SingletonInstance;
+        public override string Name { get; } = "InductiveCard";
+        public override void OnEnabled()
+        {
+            EventHandlers eventHandlers = new EventHandlers();
+            PlayerHandlers.InteractingDoor += eventHandlers.OnInteractingDoor;
+            base.OnEnabled();
+        }
+        public override void OnDisabled()
+        {
+            EventHandlers eventHandlers = new EventHandlers();
+            PlayerHandlers.InteractingDoor-=eventHandlers.OnInteractingDoor;
+            base.OnDisabled();
+        }
+    }
+}
