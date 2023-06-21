19e6f0ae5a1ee1a97a9887ea1cf98a68b48c5f0f
Vyrmedea <noilndygs@gmail.com>
2023-06-21 19:54:22 +0800

    修复了身上没卡时无法开门的bug

--- InductiveCard/EventHandlers.cs
+++ InductiveCard/EventHandlers.cs
@@ -1,6 +1,5 @@
 ﻿namespace InductiveCard
 {
-    using Exiled.API.Features.Items;
     public class EventHandlers
     {
         public void OnInteractingDoor(Exiled.Events.EventArgs.Player.InteractingDoorEventArgs ev)
@@ -8,7 +7,7 @@
             if (ev.Player == null) return;
             foreach (var item in ev.Player.Items)
             {
-                if (item != null && item is Keycard keycard && ev.Door.RequiredPermissions.CheckPermissions(keycard.Base,ev.Player.ReferenceHub))
+                if (item != null && ev.Door.RequiredPermissions.CheckPermissions(item.Base,ev.Player.ReferenceHub))
                 {
                     ev.IsAllowed = true;
                     return;
